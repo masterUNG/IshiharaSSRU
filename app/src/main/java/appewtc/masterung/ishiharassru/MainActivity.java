49021793348d4ba2a58dd55f43fa84247ea3973f
masterUNG <phrombutr@gmail.com>
2015-05-18 16:10:19 +0700

    day1

--- app/src/main/java/appewtc/masterung/ishiharassru/MainActivity.java
+++ app/src/main/java/appewtc/masterung/ishiharassru/MainActivity.java
@@ -12,7 +12,7 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
+    }   // onCreate
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -35,4 +35,4 @@ public class MainActivity extends ActionBarActivity {
 
         return super.onOptionsItemSelected(item);
     }
-}
+}   // Main Class นี่คือ คลาสหลัก เว้ยเห้ย
